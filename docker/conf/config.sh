feb91c71d70751c32408db6b2494ef3854aba6f7
Piyus Kumar <piyusgupta01@gmail.com>
2020-05-20 20:05:51 +0530

    log file format and sql statements with duration logging

--- docker/conf/config.sh
+++ docker/conf/config.sh
@@ -8,18 +8,17 @@ if [ ${DB_DEBUG} ];
     then
         echo "setting up logging..."
         # enable logging with duration
-        sed -ri "s/#log_statement = 'none'/log_statement = 'all'/g" ${PG_SETTING_FILE}
-        sed -ri "s/#log_duration = off/log_duration = on/g" ${PG_SETTING_FILE}
+        sed -ri "s/#log_min_duration_statement = .*/log_min_duration_statement = 1/g" ${PG_SETTING_FILE}
         # log statement formatting
-        sed -ri "s/#log_line_prefix = .*/log_line_prefix = '%t - %h - %d - %a '/g" ${PG_SETTING_FILE}
+        sed -ri "s/#log_line_prefix = .*/log_line_prefix = '%t - %h - %d - %u - %a - %p : '/g" ${PG_SETTING_FILE}
     else
         echo "disabled logging..."
 fi
-
+sed -ri "s/shared_preload_libraries = .*/shared_preload_libraries = 'timescaledb, pg_stat_statements'/" ${PG_SETTING_FILE}
 # add the following config to bottom of the postgresql.conf
 {
-    echo "shared_preload_libraries = 'pg_stat_statements'"
+    # echo "shared_preload_libraries = 'pg_stat_statements'"
     echo "pg_stat_statements.max = 10000"
-    echo "pg_stat_statements.track = all"
+    echo "pg_stat_statements.track = top"
     echo "#happy postgres.ing ...:)"
 } >> ${PG_SETTING_FILE}
