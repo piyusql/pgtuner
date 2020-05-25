d0da39014667baa62965f28656465dc2287ad578
Piyus Kumar <piyusgupta01@gmail.com>
2020-05-25 22:40:50 +0530

    added script to load meminfo

--- backend/sqls/0011-resource-stats.sql
+++ backend/sqls/0011-resource-stats.sql
@@ -18,7 +18,26 @@ OPTIONS
 (filename '/proc/meminfo', format 'csv', delimiter ':');
 
 -- Put a record in metrics, expecting data will be available
+-- Add CPU Load
 INSERT INTO metrics
     (client_id, chart_id, timestamp, data)
-SELECT 1, 1, now(), json_build_object('one', one, 'five', five, 'fifteen', fifteen)
-from cpuloadavg ;
+SELECT
+    1, 1, now(), json_build_object('one', one, 'five', five, 'fifteen', fifteen)
+FROM
+    cpuloadavg;
+
+-- Add Memory Load
+INSERT INTO metrics
+    (client_id, chart_id, timestamp, data)
+SELECT
+    1, 2, now(), json_object(array_agg(stat
+ORDER BY stat
+), array_agg
+(trim
+(value)
+        ORDER BY stat))
+FROM
+    meminfo
+WHERE
+    stat IN
+('MemTotal', 'MemFree', 'SwapTotal', 'SwapFree');
